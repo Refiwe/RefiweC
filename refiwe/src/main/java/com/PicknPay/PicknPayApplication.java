3e1fe3de12bd04069f950e1dd20536993453a701
User <User@S2>
2018-04-18 10:56:56 +0200

    comment

--- refiwe/src/main/java/com/PicknPay/PicknPayApplication.java
+++ refiwe/src/main/java/com/PicknPay/PicknPayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages={"com.PicknPay","com.PicknPay.model","com.PicknPay.services","com.PicknPay.controller","com.PicknPay.repositories"})
 public class PicknPayApplication {
-//???????????????
+//??????????????? another oneSSSSSSSSSSSSSSSSSS
 	public static void main(String[] args) {
 		SpringApplication.run(PicknPayApplication.class, args);
 	}
